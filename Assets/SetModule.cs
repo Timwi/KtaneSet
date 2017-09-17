2d82e808965bd925ce572592427aa4efe37617a6
Timwi <github@timwi.de>
2017-09-18 01:16:52 +0200

    Set symbols and highlight

--- Assets/SetModule.cs
+++ Assets/SetModule.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Set;
 using UnityEngine;
+
 using Rnd = UnityEngine.Random;
 
 /// <summary>
@@ -16,25 +17,101 @@ public class SetModule : MonoBehaviour
     public KMAudio Audio;
 
     public Texture[] Symbols;
-    public GameObject CardTemplate;
+    public KMSelectable[] Cards;
+    public KMSelectable MainSelectable;
+
+    private MeshRenderer[] _cardImages;
 
     private static int _moduleIdCounter = 1;
     private int _moduleId;
+
+    struct SetCard : IEquatable<SetCard>
+    {
+        public int Index { get; private set; }
+        public int X { get { return Index % 3; } }
+        public int Y { get { return (Index / 3) % 3; } }
+        public int NumDots { get { return (Index / 9) % 3; } }
+        public int Filling { get { return Index / 27; } }
+
+        public SetCard(int x, int y, int numDots, int filling) { Index = x + 3 * (y + 3 * (numDots + 3 * filling)); }
+        public SetCard(int index) { Index = index; }
+
+        private static int get3rdValue(int one, int two) { return (6 - one - two) % 3; }
+        public SetCard Get3rdInSet(SetCard other) { return new SetCard(get3rdValue(X, other.X), get3rdValue(Y, other.Y), get3rdValue(NumDots, other.NumDots), get3rdValue(Filling, other.Filling)); }
+        public static SetCard GetRandom() { return new SetCard(Rnd.Range(0, 81)); }
+        public bool Equals(SetCard other) { return other.Index == Index; }
+        public override int GetHashCode() { return Index; }
+        public override bool Equals(object obj) { return obj is SetCard && Equals((SetCard) obj); }
+    }
+
+    sealed class SetSet : IEquatable<SetSet>
+    {
+        public SetCard One { get; private set; }
+        public SetCard Two { get; private set; }
+        public SetCard Three { get; private set; }
+        public IEnumerable<SetCard> Cards { get { yield return One; yield return Two; yield return Three; } }
+        public SetSet(SetCard one, SetCard two, SetCard three)
+        {
+            One = one;
+            Two = two;
+            Three = three;
+        }
+        public override int GetHashCode()
+        {
+            // The hash function needs to be invariant under reordering of the three cards
+            return (One.GetHashCode() + Two.GetHashCode() + Three.GetHashCode()) ^ (One.GetHashCode() * Two.GetHashCode() * Three.GetHashCode());
+        }
+        public bool Equals(SetSet other) { return other != null && other.Cards.Contains(One) && other.Cards.Contains(Two) && other.Cards.Contains(Three); }
+        public override bool Equals(object obj) { return obj is SetSet && Equals((SetSet) obj); }
+
+        public static SetSet GetRandom(bool avoidSameSymbol = false)
+        {
+            var one = SetCard.GetRandom();
+            tryAgain:
+            var two = SetCard.GetRandom();
+            if (two.Equals(one) || (avoidSameSymbol && one.X == two.X && one.Y == two.Y))
+                goto tryAgain;
+            return new SetSet(one, two, one.Get3rdInSet(two));
+        }
+    }
 
     void Start()
     {
         _moduleId = _moduleIdCounter++;
 
-        for (int i = 0; i < 12; i++)
+        // The textures are in random order and I couldn’t be bothered to fix it in Unity.
+        Symbols = Enumerable.Range(0, 81).Select(ix => Symbols.FirstOrDefault(sy => sy.name == string.Format("Icon{0}{1}{2}{3}", (char) ('A' + (ix % 3)), (char) ('1' + ((ix / 3) % 3)), (char) ('a' + ((ix / 9) % 3)), (char) ('1' + ((ix / 27) % 3))))).ToArray();
+
+        _cardImages = new MeshRenderer[Cards.Length];
+        for (int i = 0; i < Cards.Length; i++)
+            _cardImages[i] = Cards[i].transform.FindChild("CardImage").GetComponent<MeshRenderer>();
+
+        tryAgain:
+        var solution = SetSet.GetRandom(avoidSameSymbol: true);
+        var cards = new HashSet<SetCard> { solution.One, solution.Two, solution.Three };
+        var iter = 0;
+        while (cards.Count < 12)
         {
-            var card = Instantiate(CardTemplate);
-            card.GetComponent<MeshRenderer>().material.mainTexture = Symbols[i];
-            card.transform.parent = CardTemplate.transform.parent;
-            card.transform.localPosition = new Vector3(-.06f + .04f * (i % 4), .01501f, .02f - .04f * (i / 4));
-            card.transform.localEulerAngles = new Vector3(90, 0, 0);
-            card.transform.localScale = new Vector3(.04f, .04f, .04f);
-            card.name = string.Format("Card {0}{1}", (char) ('A' + (i % 4)), (char) ('1' + (i / 4)));
+            var newCard = SetCard.GetRandom();
+            if (cards.Contains(newCard) || cards.Any(c => cards.Contains(newCard.Get3rdInSet(c))))
+            {
+                iter++;
+                if (iter > 1000)
+                    goto tryAgain;
+                continue;
+            }
+            cards.Add(newCard);
         }
-        CardTemplate.SetActive(false);
+
+        var cardsOrdered = cards.ToList().Shuffle();
+        for (int i = 0; i < cardsOrdered.Count; i++)
+            _cardImages[i].material.mainTexture = Symbols[cardsOrdered[i].Index];
+
+        Debug.LogFormat("Solution: {0}, {1}, {2}", cardsOrdered.IndexOf(solution.One), cardsOrdered.IndexOf(solution.Two), cardsOrdered.IndexOf(solution.Three));
+    }
+
+    private static IEnumerable<int> GetRandomDimensionValues()
+    {
+        return (Rnd.Range(0, 2) == 0 ? Enumerable.Repeat(Rnd.Range(0, 3), 3) : Enumerable.Range(0, 3).ToList().Shuffle());
     }
 }
---
+++ Assets/Ut.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Rnd = UnityEngine.Random;
+
+namespace Set
+{
+    static class Ut
+    {
+        /// <summary>
+        ///     Brings the elements of the given list into a random order.</summary>
+        /// <typeparam name="T">
+        ///     Type of elements in the list.</typeparam>
+        /// <param name="list">
+        ///     List to shuffle.</param>
+        /// <returns>
+        ///     The list operated on.</returns>
+        public static IList<T> Shuffle<T>(this IList<T> list)
+        {
+            if (list == null)
+                throw new ArgumentNullException("list");
+            for (int j = list.Count; j >= 1; j--)
+            {
+                int item = Rnd.Range(0, j);
+                if (item < j - 1)
+                {
+                    var t = list[item];
+                    list[item] = list[j - 1];
+                    list[j - 1] = t;
+                }
+            }
+            return list;
+        }
+
+        public static T[] NewArray<T>(params T[] array) { return array; }
+
+        /// <summary>
+        ///     Instantiates a fully-initialized array with the specified dimensions.</summary>
+        /// <param name="size">
+        ///     Size of the first dimension.</param>
+        /// <param name="initialiser">
+        ///     Function to initialise the value of every element.</param>
+        /// <typeparam name="T">
+        ///     Type of the array element.</typeparam>
+        public static T[] NewArray<T>(int size, Func<int, T> initialiser)
+        {
+            if (initialiser == null)
+                throw new ArgumentNullException("initialiser");
+            var result = new T[size];
+            for (int i = 0; i < size; i++)
+                result[i] = initialiser(i);
+            return result;
+        }
+
+        /// <summary>
+        ///     Instantiates a fully-initialized rectangular jagged array with the specified dimensions.</summary>
+        /// <param name="size1">
+        ///     Size of the first dimension.</param>
+        /// <param name="size2">
+        ///     Size of the second dimension.</param>
+        /// <param name="initialiser">
+        ///     Optional function to initialise the value of every element.</param>
+        /// <typeparam name="T">
+        ///     Type of the array element.</typeparam>
+        public static T[][] NewArray<T>(int size1, int size2, Func<int, int, T> initialiser = null)
+        {
+            var result = new T[size1][];
+            for (int i = 0; i < size1; i++)
+            {
+                var arr = new T[size2];
+                if (initialiser != null)
+                    for (int j = 0; j < size2; j++)
+                        arr[j] = initialiser(i, j);
+                result[i] = arr;
+            }
+            return result;
+        }
+
+        /// <summary>
+        ///     Turns all elements in the enumerable to strings and joins them using the specified <paramref
+        ///     name="separator"/> and the specified <paramref name="prefix"/> and <paramref name="suffix"/> for each string.</summary>
+        /// <param name="values">
+        ///     The sequence of elements to join into a string.</param>
+        /// <param name="separator">
+        ///     Optionally, a separator to insert between each element and the next.</param>
+        /// <param name="prefix">
+        ///     Optionally, a string to insert in front of each element.</param>
+        /// <param name="suffix">
+        ///     Optionally, a string to insert after each element.</param>
+        /// <param name="lastSeparator">
+        ///     Optionally, a separator to use between the second-to-last and the last element.</param>
+        /// <example>
+        ///     <code>
+        ///         // Returns "[Paris], [London], [Tokyo]"
+        ///         (new[] { "Paris", "London", "Tokyo" }).JoinString(", ", "[", "]")
+        ///         
+        ///         // Returns "[Paris], [London] and [Tokyo]"
+        ///         (new[] { "Paris", "London", "Tokyo" }).JoinString(", ", "[", "]", " and ");</code></example>
+        public static string JoinString<T>(this IEnumerable<T> values, string separator = null, string prefix = null, string suffix = null, string lastSeparator = null)
+        {
+            if (values == null)
+                throw new ArgumentNullException("values");
+            if (lastSeparator == null)
+                lastSeparator = separator;
+
+            using (var enumerator = values.GetEnumerator())
+            {
+                if (!enumerator.MoveNext())
+                    return "";
+
+                // Optimise the case where there is only one element
+                var one = enumerator.Current;
+                if (!enumerator.MoveNext())
+                    return prefix + one + suffix;
+
+                // Optimise the case where there are only two elements
+                var two = enumerator.Current;
+                if (!enumerator.MoveNext())
+                {
+                    // Optimise the (common) case where there is no prefix/suffix; this prevents an array allocation when calling string.Concat()
+                    if (prefix == null && suffix == null)
+                        return one + lastSeparator + two;
+                    return prefix + one + suffix + lastSeparator + prefix + two + suffix;
+                }
+
+                StringBuilder sb = new StringBuilder()
+                    .Append(prefix).Append(one).Append(suffix).Append(separator)
+                    .Append(prefix).Append(two).Append(suffix);
+                var prev = enumerator.Current;
+                while (enumerator.MoveNext())
+                {
+                    sb.Append(separator).Append(prefix).Append(prev).Append(suffix);
+                    prev = enumerator.Current;
+                }
+                sb.Append(lastSeparator).Append(prefix).Append(prev).Append(suffix);
+                return sb.ToString();
+            }
+        }
+
+        public static T PickRandom<T>(this IEnumerable<T> src)
+        {
+            if (src == null)
+                throw new ArgumentNullException("src");
+
+            var arr = src.ToArray();
+            if (arr.Length == 0)
+                throw new InvalidOperationException("Cannot pick a random element from an empty set.");
+            return arr[Rnd.Range(0, arr.Length)];
+        }
+    }
+}
