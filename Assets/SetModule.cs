dcdda504f20ee50afa639dc1c889314838ed7618
Timwi <github@timwi.de>
2025-09-20 01:56:23 +0200

    Rule-seed support

--- Assets/SetModule.cs
+++ Assets/SetModule.cs
@@ -17,18 +17,22 @@ public class SetModule : MonoBehaviour
     public KMBombInfo Bomb;
     public KMBombModule Module;
     public KMAudio Audio;
+    public KMRuleSeedable RuleSeedable;
 
     public Texture[] Symbols;
+    public Texture[] Dots;
     public Texture[] SymbolsSelected;
+    public Texture[] DotsSelected;
     public KMSelectable[] Cards;
     public KMSelectable MainSelectable;
 
-    private MeshRenderer[] _cardImages;
-    private MeshRenderer[] _cardSelections;
+    public MeshRenderer[] CardImages;
+    public MeshRenderer[] CardDots;
 
     private static int _moduleIdCounter = 1;
     private int _moduleId;
-    private List<int> _selected = new List<int>();
+    private readonly List<int> _selected = new List<int>();
+    private int[] _symbols;
     private SetSet _solution;
     private SetCard[] _displayedCards;
     private bool _isSolved;
@@ -36,10 +40,10 @@ public class SetModule : MonoBehaviour
     struct SetCard : IEquatable<SetCard>
     {
         public int Index { get; private set; }
-        public int X { get { return Index % 3; } }
-        public int Y { get { return (Index / 3) % 3; } }
-        public int NumDots { get { return (Index / 9) % 3; } }
-        public int Filling { get { return Index / 27; } }
+        public int X => Index % 3;
+        public int Y => (Index / 3) % 3;
+        public int NumDots => (Index / 9) % 3;
+        public int Filling => Index / 27;
 
         public SetCard(int x, int y, int numDots, int filling) { Index = x + 3 * (y + 3 * (numDots + 3 * filling)); }
         public SetCard(int index) { Index = index; }
@@ -50,6 +54,7 @@ public class SetModule : MonoBehaviour
         public bool Equals(SetCard other) { return other.Index == Index; }
         public override int GetHashCode() { return Index; }
         public override bool Equals(object obj) { return obj is SetCard && Equals((SetCard) obj); }
+        public override string ToString() => $"X={X}, Y={Y}, NumDots={NumDots}, Filling={Filling}";
     }
 
     sealed class SetSet : IEquatable<SetSet>
@@ -87,18 +92,15 @@ public class SetModule : MonoBehaviour
     {
         _moduleId = _moduleIdCounter++;
 
-        // The textures are in random order and I couldn’t be bothered to fix it in Unity.
-        Symbols = Enumerable.Range(0, 81).Select(ix => Symbols.FirstOrDefault(sy => sy.name == string.Format("Icon{0}{1}{2}{3}", (char) ('A' + (ix % 3)), (char) ('1' + ((ix / 3) % 3)), (char) ('a' + ((ix / 9) % 3)), (char) ('1' + ((ix / 27) % 3))))).ToArray();
-        SymbolsSelected = Enumerable.Range(0, 81).Select(ix => SymbolsSelected.FirstOrDefault(sy => sy.name == string.Format("IconSel{0}{1}{2}{3}", (char) ('A' + (ix % 3)), (char) ('1' + ((ix / 3) % 3)), (char) ('a' + ((ix / 9) % 3)), (char) ('1' + ((ix / 27) % 3))))).ToArray();
-
-        _cardImages = new MeshRenderer[Cards.Length];
-        _cardSelections = new MeshRenderer[Cards.Length];
         for (int i = 0; i < Cards.Length; i++)
-        {
-            _cardImages[i] = Cards[i].transform.Find("CardImage").GetComponent<MeshRenderer>();
-            _cardSelections[i] = Cards[i].transform.Find("CardSelection").GetComponent<MeshRenderer>();
             Cards[i].OnInteract = getClickHandler(i);
-        }
+
+        var rnd = RuleSeedable.GetRNG();
+        _symbols = rnd.Seed == 1
+            ? Enumerable.Range(0, 9).ToArray()
+            : rnd.ShuffleFisherYates(Enumerable.Range(0, Symbols.Length / 3).ToArray()).Take(9).ToArray();
+
+        Debug.LogFormat("[S.E.T. #{0}] Using rule seed: {1}", _moduleId, rnd.Seed);
 
         tryAgain:
         _solution = SetSet.GetRandom(avoidSameSymbol: true);
@@ -118,67 +120,66 @@ public class SetModule : MonoBehaviour
         }
 
         _displayedCards = cards.ToList().Shuffle().ToArray();
-        var fillings = new[] { "filled", "wavy", "empty" };
+        Debug.LogFormat("[S.E.T. #{0}] Icons in manual: {1}", _moduleId, _symbols.JoinString(", "));
+        Debug.LogFormat("[S.E.T. #{0}] Icons on module: {1}", _moduleId, _displayedCards.Select(card => $"{card.X + 3 * card.Y}/{card.Filling}/{card.NumDots}").JoinString(", "));
         for (int i = 0; i < _displayedCards.Length; i++)
-        {
-            Debug.LogFormat("[S.E.T. #{0}] Icon at (module) {1}{2} is (manual) {3}{4}, {5}, {6} dots.", _moduleId,
-                (char) ('A' + i % 3), (char) ('1' + i / 3),
-                (char) ('A' + _displayedCards[i].X), (char) ('1' + _displayedCards[i].Y),
-                fillings[_displayedCards[i].Filling], _displayedCards[i].NumDots);
-            _cardImages[i].material.mainTexture = Symbols[_displayedCards[i].Index];
-        }
-
+            SetSymbol(i, false);
         Debug.LogFormat("[S.E.T. #{0}] Solution: {1}, {2}, {3}.",
             _moduleId, coords(Array.IndexOf(_displayedCards, _solution.One)), coords(Array.IndexOf(_displayedCards, _solution.Two)), coords(Array.IndexOf(_displayedCards, _solution.Three)));
     }
 
-    private string coords(int i)
+    private void SetSymbol(int btn, bool selected)
     {
-        return (char) ('A' + i % 3) + "" + (char) ('1' + i / 3);
+        var setCard = _displayedCards[btn];
+        var sym = selected ? SymbolsSelected : Symbols;
+        var dots = selected ? DotsSelected : Dots;
+
+        CardDots[btn].gameObject.SetActive(setCard.NumDots > 0);
+        CardDots[btn].material.mainTexture = setCard.NumDots > 0 ? dots[setCard.NumDots - 1] : null;
+        CardImages[btn].material.mainTexture = sym[setCard.Filling + 3 * _symbols[setCard.X + 3 * setCard.Y]];
     }
 
-    private KMSelectable.OnInteractHandler getClickHandler(int i)
+    private string coords(int i) => $"{(char) ('A' + i % 3)}{(char) ('1' + i / 3)}";
+
+    private KMSelectable.OnInteractHandler getClickHandler(int btn)
     {
         return delegate
         {
-            Cards[i].AddInteractionPunch();
+            Cards[btn].AddInteractionPunch();
 
             if (_isSolved)
             {
-                Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, Cards[i].transform);
+                Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, Cards[btn].transform);
                 return false;
             }
 
-            if (_selected.Contains(i))
+            if (_selected.Contains(btn))
             {
-                Audio.PlaySoundAtTransform("Deselect", Cards[i].transform);
-                //_cardSelections[i].material = CardUnselected;
-                _cardImages[i].material.mainTexture = Symbols[_displayedCards[i].Index];
-                _selected.Remove(i);
+                Audio.PlaySoundAtTransform("Deselect", Cards[btn].transform);
+                SetSymbol(btn, false);
+                _selected.Remove(btn);
             }
             else
             {
-                _selected.Add(i);
+                _selected.Add(btn);
                 if (_selected.Count == 3 && _selected.All(s => _solution.Cards.Contains(_displayedCards[s])))
                 {
                     Debug.LogFormat("[S.E.T. #{0}] Module solved.", _moduleId);
                     Module.HandlePass();
-                    //_cardSelections[i].material = CardSelected;
-                    _cardImages[i].material.mainTexture = SymbolsSelected[_displayedCards[i].Index];
+                    SetSymbol(btn, true);
                     _isSolved = true;
-                    Audio.PlaySoundAtTransform("Chime", Cards[i].transform);
+                    Audio.PlaySoundAtTransform("Chime", Cards[btn].transform);
                 }
                 else if (_selected.Count == 3)
                 {
                     Debug.LogFormat("[S.E.T. #{0}] Incorrect set selected: {1}, {2}, {3}.", _moduleId, coords(_selected[0]), coords(_selected[1]), coords(_selected[2]));
                     Module.HandleStrike();
-                    _selected.Remove(i);
+                    _selected.Remove(btn);
                 }
                 else
                 {
-                    //_cardSelections[i].material = CardSelected;
-                    _cardImages[i].material.mainTexture = SymbolsSelected[_displayedCards[i].Index];
-                    Audio.PlaySoundAtTransform("Stamp", Cards[i].transform);
+                    SetSymbol(btn, true);
+                    Audio.PlaySoundAtTransform("Stamp", Cards[btn].transform);
                 }
             }
             return false;
